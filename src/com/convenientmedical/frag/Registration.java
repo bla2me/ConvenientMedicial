4ca0670bc4a7eda4be7977c3029ee3acae82fcaa
Mr.Codey <1102768969@qq.com>
2015-07-02 17:33:43 +0800

    修改

--- src/com/convenientmedical/frag/Registration.java
+++ src/com/convenientmedical/frag/Registration.java
@@ -1,5 +1,6 @@
 package com.convenientmedical.frag;
 
+import com.convenientmedical.adapter.MyExpandablelistViewAdapter;
 import com.convenientmedical.main.R;
 
 import android.app.Fragment;
@@ -7,21 +8,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
 
 /**
- * @author Mr.Codey
- *  挂号fragment
+ * @author Mr.Codey 挂号fragment
  */
 public class Registration extends Fragment {
-	
-private View registrationView;
+
+	private View registrationView;
+	private ExpandableListView mListView;
+	private EditText metSearch;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		registrationView=inflater.inflate(R.layout.registration, container,false);
+		registrationView = inflater.inflate(R.layout.registration, container,
+				false);
+		initView();
+		mListView.setAdapter(new MyExpandablelistViewAdapter(getActivity()));
+		mListView.setGroupIndicator(null);
 		return registrationView;
 	}
 
+	private void initView() {
+		mListView=(ExpandableListView)registrationView.findViewById(R.id.expandableListView_doc_or_hos);
+		metSearch=(EditText)registrationView.findViewById(R.id.Et_Search_doc_Tips);
+	}
 }
--- src/com/convenientmedical/main/LogIn.java
+++ src/com/convenientmedical/main/LogIn.java
@@ -1,8 +1,13 @@
 package com.convenientmedical.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * 登录
@@ -10,15 +15,55 @@ import android.os.Bundle;
  * @author Mr.Codey
  *
  */
-public class LogIn extends Activity {
-	private static final int STATUS=1 ;//登录状态值
-	private SharedPreferences preferences;//保存登录状态和cookies
+public class LogIn extends Activity implements OnClickListener {
+	private static final int LOG_IN_STATUS = 1;// 登录状态值
+	private SharedPreferences preferences;// 保存登录状态和cookies
+	private Button mbtLogIn, mbtSignup, mbtNoLogIn;
+	private EditText mUserName, mPwd;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.log_in);
+		initView();
+	}
+
+	/**
+	 * 实例化控件
+	 */
+	private void initView() {
+		mbtSignup = (Button) findViewById(R.id.Bt_register);
+		mbtNoLogIn = (Button) findViewById(R.id.Bt_nolanding);
+		mbtLogIn = (Button) findViewById(R.id.BT_landing);
+		mUserName = (EditText) findViewById(R.id.Et_username);
+		mPwd = (EditText) findViewById(R.id.Et_password);
+		mbtSignup.setOnClickListener(this);
+		mbtNoLogIn.setOnClickListener(this);
+		mbtLogIn.setOnClickListener(this);
+	}
+
+	/**
+	 * 点击事件处理
+	 */
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent();
+		switch (v.getId()) {
+		case R.id.Bt_register:
+			intent.setClass(getApplicationContext(), SignUpActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.Bt_nolanding:
+			intent.setClass(getApplicationContext(), MainActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.BT_landing:
+		default:
+			intent.setClass(getApplicationContext(), MainActivity.class);
+			break;
+		}
 	}
 
 }
